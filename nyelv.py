d7eec197c414151c293ec320604ccd6f134ffbc0
Kelvin Jackson <kechpaja@comcast.net>
2018-09-01 05:21:03 -0400

    Working version of GET half of nyelv site

--- nyelv.py
+++ nyelv.py
@@ -13,7 +13,7 @@ def genpage(user, query):
     acc = "<html>\n    <head>\n        <title>" + user + " speaks:</title>"
     acc += '''
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <lnk rel="stylesheet" type="text/css" href="../styles.css">
+        <link rel="stylesheet" type="text/css" href="../css/styles.css">
     </head>
 
     <body>
@@ -24,15 +24,18 @@ def genpage(user, query):
     blocks = []
     for level in ["N", "C", "B", "A"]:
         blocks.append("\n".join([mkrow(l) for l in query if l[2] == level]))
-    acc += "\n\n<tr class=\"border\"></td></td><td></td></tr>\n\n".join(blocks)
+    blocks = [blocks[0] + blocks[1], blocks[2], blocks[3]]
+    blocks = [b for b in blocks if len(b) > 0]
+    acc += "\n\n<tr class=\"border\"><td></td><td></td></tr>\n\n".join(blocks)
 
     acc += '''
         </table>
 
-        <p>Key: <span class="fluent">fluent</span>, 
-        <span class="intermediate">intermediate</span>, 
-        <span class="beginner">beginner</span>. Native
-        languages are starred (<span class="native">&bigstar;</span>).</p>
+        <p>
+        Key: <span class="C">fluent</span>, <span class="B">intermediate</span>,
+        <span class="A">beginner</span>. Native languages are starred 
+        (<span class="left-column">&bigstar;</span>).
+        </p>
 
     </body>
 </html>
@@ -42,14 +45,7 @@ def genpage(user, query):
 class LanguageListResource(object):
     def __init__(self, connection):
         self.connection = connection
-        self.cursor = self.connection.cursor()
-        
-        # TODO do all of this elsewhere
-        #self.connection.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
-        #self.connection.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
-
-        #self.cursor = self.connection.cursor()
-        #self.cursor.execute("use nyelv;") # TODO variable?
+        self.cursor = self.connection.cursor() 
 
     def on_get(self, req, resp, user):
         sql = '''select
@@ -58,13 +54,12 @@ class LanguageListResource(object):
                     whospeakswhat.level
                 from languages join whospeakswhat 
                     on whospeakswhat.language = languages.id
-                where (whospeakswhat.user = ?)'''
-        self.cursor.execute(sql, user)
-        resp.body = genpage(self.cursor.fetchall()) # TODO a tad dangerous?
+                where (whospeakswhat.user = %s)'''
+        self.cursor.execute(sql, (user, ))
+        resp.body = genpage(user, self.cursor.fetchall()) # TODO dangerous?
 
         resp.content_type = "text/html; charset=utf-8"
         resp.status = falcon.HTTP_200
-
 
 ####
 # Start everything up
@@ -73,7 +68,8 @@ class LanguageListResource(object):
 # Set up database connection
 db = MySQLdb.connect(host="nyelv.db",
                      db="nyelv",
-                     read_default_file="~/.nyelv-db.conf",
+                     user="nyelv",
+                     read_default_file="/home/protected/.nyelv-db.conf",
                      charset="utf8")
 
 # Start Falcon
