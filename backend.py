510f7aa783deaff17595d82e96d9a5f4aaa027eb
Kelvin Jackson <kechpaja@comcast.net>
2018-08-31 21:38:01 -0400

    Part of the database query is set up for get

--- backend.py
+++ backend.py
@@ -1,9 +1,7 @@
 import falcon
 
-# TODO Update CSS to match what we have here
-
 def filterquery(query, level):
-    return [[x[1], x[2], x[3]] for x in query if x[3] == level]
+    return [x for x in query if x[2] == level]
 
 def mkrow(lang):
     acc = "<tr class=\"" + lang[2] + "\"><td class=\"left-column\">"
@@ -15,7 +13,7 @@ def mkrow(lang):
 
 def genpage(user, query):
     # Expect query to be a list of lists or tuples containing the
-    # username, language code, language, and level
+    # language code, language, and level
     acc = "<html>\n    <head>\n        <title>" + user + " speaks:</title>"
     acc += '''
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -45,14 +43,31 @@ def genpage(user, query):
 '''
     return acc
 
-
 class LanguageListResource(object):
-    def on_get(self, req, resp):
-        # TODO connect to database
-        # TODO get languages associated with user
-        # TODO generate display page
-        # TODO return
-        pass # TODO
+    def __init__(self, connection):
+        self.connection = connection
+        self.cursor = self.connection.cursor()
+        
+        # TODO do all of this elsewhere
+        #self.connection.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
+        #self.connection.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
+
+        #self.cursor = self.connection.cursor()
+        #self.cursor.execute("use nyelv;") # TODO variable?
+
+    def on_get(self, req, resp, user):
+        sql = '''select
+                    languages.id,
+                    languages.name,
+                    whospeakswhat.level
+                from languages join whospeakswhat 
+                    on whospeakswhat.language = languages.id
+                where (whospeakswhat.user = ?)'''
+        self.cursor.execute(sql, user)
+        resp.body = genpage(self.cursor.fetchall()) # TODO a tad dangerous?
+
+        resp.content_type = "text/html; charset=utf-8"
+        resp.status = falcon.HTTP_200
 
 
 ####
