06c0110278528c9aec5598339e9404ab037a5c83
Kelvin Jackson <kechpaja@comcast.net>
2018-09-07 17:33:13 -0700

    Beginnings of email sending

--- src/py/email.py
+++ src/py/email.py
@@ -2,9 +2,23 @@
 # Emails
 #######
 
+import requests
+
 class Mailer(object):
     def __init__(self):
-        pass # TODO get API token from file, etc
+        self.apikey = "" # TODO get API token from file, etc
+        self.uribase = "https://api.elasticemail.com/v2/"
+
+    def mkreq(self, verb, options):
+        newopts = dict((o, options[o]) for o in options)
+        newopts["apikey"] = self.apikey
+        return requests.get(self.uribase + verb, params=newopts)
 
     def send_link(self, token, address):
-        print("Send link with token to " + address) # TODO 
+        options = {}
+        options["msgTo"] = address
+
+        # TODO Probably should give user more than just the link
+        options["bodyText"] = "https://kechpaja.com/langlist?token=" + token
+
+        resp = self.mkreq("email/send", options)
