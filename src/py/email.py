9f32b17c242b215fb59fd023d2f798172ec3b1c4
Kelvin Jackson <kechpaja@comcast.net>
2018-09-09 17:22:46 -0700

    Simply email module

--- src/py/email.py
+++ src/py/email.py
@@ -1,18 +1,13 @@
-#######
-# Emails
-#######
+################################################################################
+# Email Sending Library                                                        #
+################################################################################
 
 import os
 
-def sendmsg(headers, body):
-    hdrs = "\n".join([h + ": " + headers[h] for h in headers.keys()])
-    with os.popen("/usr/bin/sendmail -t", "w") as f:
-        f.write(hdrs + "\n\n" + body)
-
 def send_link(token, address):
-    headers = {
-        "To": address,
-        "Subject": "Login Link"
-    }
-    body = "Login link: https://kechpaja.com/langlist?token=" + token
-    sendmsg(headers, body)
+    msg = '''Subject: Login Link
+To: %s
+
+Login link: https://kechpaja.com/langlist?token=%s''' % (address, token)
+    with os.popen("/usr/bin/sendmail -t", "w") as f:
+        f.write(msg)
