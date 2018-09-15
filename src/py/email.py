5dec711e8d9f884237f5b58ec390927574f98ef0
Kelvin Jackson <kechpaja@comcast.net>
2018-09-15 01:47:49 -0700

    Finish transition to TalkToMeIn name

--- src/py/email.py
+++ src/py/email.py
@@ -9,6 +9,6 @@ def send_link(token, address):
 From: kechpaja@kechpaja.com
 To: %s
 
-Login link: https://myalect.com/?token=%s''' % (address, token)
+Login link: https://talktomein.com/?token=%s''' % (address, token)
     with os.popen("/usr/bin/sendmail -t", "w") as f:
         f.write(msg)
