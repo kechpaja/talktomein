f5bf3b59b6e2d873b8e07e5057ab41a4e59b546f
Kelvin Jackson <kechpaja@comcast.net>
2018-09-16 12:32:21 -0700

    No need to re-deploy the run script every time

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,3 @@ basedir="$(dirname $(readlink -f "$0"))"
 scp $basedir/src/py/* $sitelogin:/home/protected/pyenv/app
 scp $basedir/src/js/* $sitelogin:/home/public/js
 scp $basedir/src/css/* $sitelogin:/home/public/css
-scp $basedir/src/run.sh $sitelogin:/home/protected
