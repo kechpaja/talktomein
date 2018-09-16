df1de5b3c61f47c74dcf48c9bff6218d0bd4135c
Kelvin Jackson <kechpaja@comcast.net>
2018-09-16 12:26:03 -0700

    Updated deployment framework

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-sitelogin="" # TODO
+sitelogin=kechpaja_talktomein@ssh.phx.nearlyfreespeech.net
 
 basedir="$(dirname $(readlink -f "$0"))"
 
-scp $basedir/src/py/* $sitelogin:/home/protected/env/src
+scp $basedir/src/py/* $sitelogin:/home/protected/pyenv/app
 scp $basedir/src/js/* $sitelogin:/home/public/js
 scp $basedir/src/css/* $sitelogin:/home/public/css
+scp $basedir/src/run.sh $sitelogin:/home/protected
