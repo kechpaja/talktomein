83d7e1ddbc41cc993df54f49d5e860de3087bfd5
Kelvin Jackson <kechpaja@comcast.net>
2018-09-21 02:41:24 -0700

    We now use stateless tokens for login links and sessions

--- src/py/middleware.py
+++ src/py/middleware.py
@@ -2,6 +2,8 @@
 # Middleware for session management in Nyelv
 #####
 
+from itsdangerous import URLSafeTimedSerializer, BadSignature, SignatureExpired
+
 from .db import DatabaseWrapper
 
 cookiename = "talktomein-session"
@@ -19,27 +21,34 @@ class DatabaseMiddleware(object):
 
 # TODO Revisit this entire class. It could use cleanup. 
 class SessionMiddleware(object):
+    def __init__(self, secret):
+        self.login_signer = URLSafeTimedSerializer(secret, salt="login")
+        self.session_signer = URLSafeTimedSerializer(secret, salt="session")
+
     # "then" must be a lambda taking one argument, the user ID
     def set_user_then(self, req, then=lambda user: None):
-        user = req.context["db"].get_token_user("login", req.params["token"])
-        if not user:
+        try:
+            req.context["user"] = self.login_signer.loads(req.params["token"],
+                                                          max_age=600)
+        except BadSignature:
+            # TODO what do we do here? Tell user why token failed?
             return False
-        req.context["user"] = user
-        then(user)
+        except SignatureExpired:
+            return False # TODO probably tell user about this one
+        then(req.context["user"])
         return True
 
     def process_request(self, req, resp):
         if req.path in ["/", "/account/delete"]:
             def set_cookie(user):
                 resp.set_cookie(cookiename,
-                                req.context["db"].add_token("session", user),
+                                self.session_signer.dumps(user),
                                 domain="talktomein.com",
                                 path="/",
-                                max_age=2419200, # XXX 28 days
+                                max_age=21600, # XXX 6 hours
                                 http_only=False)
 
             if "token" in req.params and self.set_user_then(req, set_cookie):
-                req.context["db"].delete_token("login", req.params["token"])
                 del req.params["token"]
                 if "email" in req.params and req.params["email"]:
                     # Add user or update user email
@@ -50,17 +59,20 @@ class SessionMiddleware(object):
             elif cookiename in req.cookies:
                 cookie = req.cookies[cookiename]
                 if "action" in req.params and req.params["action"] == "logout":
-                    req.context["db"].delete_token("session", cookie)
                     resp.unset_cookie(cookiename)
                 else:
-                    user = req.context["db"].get_token_user("session", cookie)
-                    if user:
-                        req.context["user"] = user
-                    else:
+                    try:
+                        req.context["user"] = self.session_signer.loads(
+                            cookie,
+                            max_age=21600
+                        )
+                    except BadSignature:
+                        resp.unset_cookie(cookiename) # TODO this is troubling
+                    except SignatureExpired:
                         resp.unset_cookie(cookiename)
+
         elif req.path in ["/account/delete/confirm", "/account/delete/finish"]:
             if "token" in req.params and self.set_user_then(req):
-                req.context["db"].delete_token("login", req.params["token"])
                 del req.params["token"]
             else:
                 raise falcon.HTTPMovedPermanently("/") # TODO "auth required"?
