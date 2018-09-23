47b5074aaa5a181cfe7a8a1847779d83afb9a3ca
Kelvin Jackson <kechpaja@comcast.net>
2018-09-22 20:21:49 -0700

    Added bit about security risks to login emails

--- src/py/send.py
+++ src/py/send.py
@@ -10,6 +10,10 @@ From: kechpaja@kechpaja.com
 To: %s
 
 %s: https://talktomein.com%s
+
+If you receive several of these messages that you did not yourself send,
+it may indicate that someone is attempting to hack into your account. If
+you are concerned, feel free to contact us by replying to this message.
 '''
 
 def link(to, subject, msg, path, token=None):
