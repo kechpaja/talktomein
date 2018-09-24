3250a5f82058e5f96a3afc4ef14f8bc461f831d2
Kelvin Jackson <kechpaja@comcast.net>
2018-09-23 18:45:49 -0700

    Language select list now sorted

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,14 @@
 (function () {
+    function insertIndex(collection, item, getSortByText) {
+        var array = [].slice.call(collection);
+        array.push(item);
+        array.sort(function (x, y) {
+            return getSortByText(x).localeCompare(getSortByText(y));
+        });
+        return array.indexOf(item);
+    }
+
+
     // Enable remove buttons
     function enableRemoveButton(button) {
         var row = button.parentElement.parentElement;
@@ -9,7 +19,17 @@
             optrow.value = row.id;
             var content = row.getElementsByClassName("language")[0].textContent;
             optrow.innerHTML = content;
-            document.getElementById("add").appendChild(optrow);
+
+            var dropdown = document.getElementById("add");
+            var index = insertIndex(dropdown.options, optrow, function (x) {
+                return x.textContent;
+            });
+
+            if (index + 1 >= dropdown.options.length) {
+                dropdown.appendChild(optrow);
+            } else {
+                dropdown.insertBefore(optrow, dropdown.options[index]);
+            }
 
             document.getElementById("save-button").disabled = false;
         };
@@ -42,20 +62,16 @@
             inner += "<td><button class='remove-button'>X</button></td></tr>";
             row.innerHTML = inner;
 
-
-            var rows = [].slice.call(document.getElementsByClassName(level));
-            rows.push(row);
-            rows.sort(function (e1, e2) {
-                var s1 = e1.getElementsByClassName("language")[0].textContent;
-                var s2 = e2.getElementsByClassName("language")[0].textContent;
-                return s1.localeCompare(s2);
+            var rows = document.getElementsByClassName(level);
+            var index = insertIndex(rows, row, function (x) {
+                return x.getElementsByClassName("language")[0].textContent;
             });
 
-            var index = rows.indexOf(row);
+            var nextRow;
             if (index + 1 >= rows.length) {
-                var nextRow = document.getElementsByClassName(before)[0];
+                nextRow = document.getElementsByClassName(before)[0];
             } else {
-                var nextRow = document.getElementsByClassName(level)[index];
+                nextRow = document.getElementsByClassName(level)[index];
             }
 
             nextRow.parentElement.insertBefore(row, nextRow);
