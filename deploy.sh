1b1c2e46f186fcf4c1ca2e0049b93b590fe300a6
Kelvin Jackson <kechpaja@comcast.net>
2019-01-12 10:01:04 +0200

    Delete old deploy script

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-sitelogin=kechpaja_talktomein@ssh.phx.nearlyfreespeech.net
-
-basedir="$(dirname $(readlink -f "$0"))"
-
-scp -r $basedir/src/py/* $sitelogin:/home/protected/pyenv/app
-scp $basedir/src/js/* $sitelogin:/home/public/js
-scp $basedir/src/css/* $sitelogin:/home/public/css
